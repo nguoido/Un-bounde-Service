ddeef8c1062bb69189c3f7572b9535bca262c96b
Do Khai <Khai.Do@windriver.com>
2020-12-31 11:06:48 +0700

    ok

--- app/src/main/java/com/dkhai/playsongservice/MainActivity.java
+++ app/src/main/java/com/dkhai/playsongservice/MainActivity.java
@@ -3,12 +3,52 @@ package com.dkhai.playsongservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonPlay;
+    private Button buttonStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonPlay = (Button) this.findViewById(R.id.button_play);
+        buttonStop = (Button) this.findViewById(R.id.button_stop);
+
+        buttonPlay.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fn_playSong();
+            }
+        });
+
+        buttonStop.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fn_stopSong();
+            }
+        });
+    }
+
+    // This method is called when users click on the Play button.
+    private void fn_playSong() {
+        // Create Intent object for PlaySongService.
+        Intent myIntent = new Intent(MainActivity.this, PlaySongService.class);
+        // myIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // Call startService with Intent parameter.
+        this.startService(myIntent);
+    }
+
+    // This method is called when users click on the Stop button.
+    private void fn_stopSong() {
+        // Create Intent object
+        Intent myIntent = new Intent(MainActivity.this, PlaySongService.class);
+        this.stopService(myIntent);
     }
 }
--- app/src/main/java/com/dkhai/playsongservice/PlaySongService.java
+++ app/src/main/java/com/dkhai/playsongservice/PlaySongService.java
@@ -4,13 +4,44 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import android.media.MediaPlayer;
+
 public class PlaySongService extends Service {
+
+    private MediaPlayer mediaPlayer;
+
     public PlaySongService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        // throw new UnsupportedOperationException("Not yet implemented");
+
+        // This service is unbounded
+        // So this method is never called.
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Create MediaPlayer object, to play your song.
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Play song.
+        mediaPlayer.start();
+        // return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        // Release the resources
+        mediaPlayer.release();
+        super.onDestroy();
     }
 }
