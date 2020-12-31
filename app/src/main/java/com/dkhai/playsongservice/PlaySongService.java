68cd6bfeb54f584256fcb43ade9c0a2a6f9d205b
Do Khai <Khai.Do@windriver.com>
2020-12-31 10:32:22 +0700

    init

---
+++ app/src/main/java/com/dkhai/playsongservice/PlaySongService.java
@@ -0,0 +1,16 @@
+package com.dkhai.playsongservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class PlaySongService extends Service {
+    public PlaySongService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
